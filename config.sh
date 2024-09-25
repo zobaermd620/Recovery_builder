2d497c85c27bfd1e6eea219918de07a67db4459f
zobaermd620 <zobaermd620@gmail.com>
2024-09-25 12:18:02 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,10 +6,10 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="Symphony_Z50"
+export DT_LINK="https://github.com/zobaermd620/twrp-symphonyz50" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="symphony" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
